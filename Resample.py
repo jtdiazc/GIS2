2041024279f4244c5c89c29ead87e711799dd1d2
jtdiazc <96319617+jtdiazc@users.noreply.github.com>
2021-12-22 16:56:38 -0800

    try

--- Resample.py
+++ Resample.py
@@ -1,2 +1,37 @@
 import rasterio
 from rasterio.enums import Resampling
+import os
+from osgeo import gdal
+import geopandas as gpd
+
+#Raster dir
+ras_dir=r"\\hydro-nas\Team\Projects\5635_SFEI Carbon and GHG\GIS\raster"
+
+#Vector dir
+vec_dir=r"\\hydro-nas\Team\Projects\5635_SFEI Carbon and GHG\GIS\vector"
+
+#template = rasterio.open(os.path.join(ras_dir,'Layout_Domain.tif'))
+#temp_cell_size=template.transform[0]
+
+infn = os.path.join(ras_dir,'TopoBathy_26910.tif')
+outfn = os.path.join(ras_dir,'TopoBathy_26910_10m.tif')
+
+xres=10
+yres=10
+resample_alg = 'average'
+
+options = gdal.WarpOptions(options=['tr'], xRes=xres, yRes=yres,resampleAlg=resample_alg,multithread=True)
+
+ds = gdal.Warp(outfn, infn, options=options)
+ds = None
+
+pts = gpd.read_file(os.path.join(vec_dir,'Template_Sampling_Points.shp'))
+pts = pts[['UTM_E', 'UTM_N', 'Value', 'geometry']]
+pts.index = range(len(pts))
+coords = [(x,y) for x, y in zip(pts.UTM_E, pts.UTM_N)]
+
+src=rasterio.open(outfn)
+
+# Sample the raster at every point location and store values in DataFrame
+pts['Raster Value'] = [x for x in src.sample(coords)]
+pts['Raster Value'] = probes.apply(lambda x: x['Raster Value'][0], axis=1)
