dceac8ab0bd298fd4ee9ad88f3fb937684617a2a
jtdiazc <jdiaz@hydrofocus.com>
2022-03-02 17:39:06 -0800

    Fixed bugs with layers drawing and added option to specify cross section coordinates

--- Lithology.py
+++ Lithology.py
@@ -8,12 +8,13 @@ Created on Thu Dec 19 15:12:55 2019
 ###First we import the packages. Make sure to have them installed in your Python
 import numpy as np
 import pandas as pd
+import os
 
 #Set parameters manually
 
-if False:
+if True:
     ###Are we drawing lines this run? True or False
-    Lines=False
+    Lines=True
     
     ###Are we drawing the image?
     Image_draw=False
@@ -21,7 +22,7 @@ if False:
     #Now we set the size of the size of the page
     #### 17x11 size by default 
 
-    width_size= 1100
+    width_size= 1500
     height_size= width_size/1224*792
 
     ##Now we set the margins in our page
@@ -88,8 +89,14 @@ if False:
 
 #Import parameters from CSV
 
-if True:
-    GraphPar=pd.read_csv('GraphicalParameters.csv',header=None).to_numpy()
+#Current directory
+os.chdir(r'P:\Applications\CrossSections')
+
+###Path to spreadsheet
+p2s=r"P:\Projects\5572_Dutch Slough GW Monitoring\GIS\Map\2021"
+
+if False:
+    GraphPar=pd.read_csv(os.path.join(p2s,'GraphicalParameters.csv'),header=None).to_numpy()
     Lines=np.array(GraphPar[0]=='TRUE')[0]
     Image_draw=np.array(GraphPar[1]=='TRUE')[0]
     width_size= int(GraphPar[2])
@@ -121,27 +128,43 @@ yscale=height_size-my-myb
 dleg=lbz+delta_leg
 
 #Import wells properties
-data = pd.read_csv('WellsProps.csv',encoding='utf-8-sig').to_numpy()
+data = pd.read_csv(os.path.join(p2s,'WellsProps.csv'),encoding='utf-8-sig')
+data=data.dropna(axis=1)
 
 ##Coordinates of wells
-x0=data[0,np.logical_not(np.isnan(data[0,:]))]
-y0=data[1,np.logical_not(np.isnan(data[1,:]))]
+x0=data.filter(items = [0], axis=0)
+x0=x0.to_numpy()
+x0=x0.reshape(x0.shape[1])
+y0=data.filter(items = [1], axis=0)
+y0=y0.to_numpy()
+y0=y0.reshape(y0.shape[1])
 
 
 #Layers for each well log
-from numpy import genfromtxt
-z = -genfromtxt('Depths.csv', delimiter=',').transpose()
+
+#from numpy import genfromtxt
+#z = -genfromtxt('Depths.csv', delimiter=',').transpose()
+z=pd.read_csv(os.path.join(p2s,'Depths.csv'),encoding='utf-8-sig',header=None)
+#Let's drop nas
+z=z.dropna(axis=0,how='all')
+z=z.dropna(axis=1,how='all')
+
+#Let's transpose
+z=z.to_numpy().transpose()
+
 #z=-np.array([[-12,-27,-49,-81,np.nan,np.nan,np.nan,np.nan,np.nan],
 #             [-18,-20,-21,-26,-32,-43,-42,-58,-63],
 #             [ -9,-13,-28,-56,-64,np.nan,np.nan,np.nan,np.nan]])
 
-#Let's clean z
 
-z=z[~np.isnan(z[:,0])]
 
 
 #Geologies
-geo=np.genfromtxt('Lithology.csv', delimiter=',',dtype='str').transpose()
+geo=pd.read_csv(os.path.join(p2s,'Lithology.csv'),encoding='utf-8-sig',header=None)
+geo=geo.dropna(axis=0,how='all')
+geo=geo.dropna(axis=1,how='all')
+geo=geo.to_numpy().transpose()
+#geo=np.genfromtxt('Lithology.csv', delimiter=',',dtype='str').transpose()
 #geo=np.array([["PT","SP","CL",np.nan,np.nan,np.nan,np.nan,np.nan],
 #             ["OL","PT","SM","OH","SM","SP","SM","CL"],
 #             [ "OL","PT","SM","CL",np.nan,np.nan,np.nan,np.nan,np.nan]])
@@ -149,18 +172,24 @@ geo=np.genfromtxt('Lithology.csv', delimiter=',',dtype='str').transpose()
 
 if Lines:
 ###Import geologic units
-    Layers=np.genfromtxt('GUs.csv', delimiter=',',dtype='str')
-    Layers[np.where(Layers!='')]
+    Layers=pd.read_csv(os.path.join(p2s,'GUs.csv'),encoding='utf-8-sig',header=None)
+    Layers=Layers.dropna(axis=1,how='all')
+    Layers=Layers.to_numpy()
+#    Layers=np.genfromtxt('GUs.csv', delimiter=',',dtype='str')
+#    Layers[np.where(Layers!='')]
 
 
 
 ###Depths of layers
-    GUdata=genfromtxt('GUdata.csv', delimiter=',')
+    GUdata=pd.read_csv(os.path.join(p2s,'GUdata.csv'),encoding='utf-8-sig',header=None)
+    GUdata=GUdata.dropna(axis=0,how='all')
+    GUdata=GUdata.dropna(axis=1,how='all')
+    GUdata=GUdata.to_numpy()
 
 
 ###Well names
 
-wellname=list(pd.read_csv('WellsProps.csv',encoding='utf-8-sig').dropna(axis='columns'))
+wellname=list(pd.read_csv(os.path.join(p2s,'WellsProps.csv'),encoding='utf-8-sig').dropna(axis='columns'))
 
 
 
@@ -174,7 +203,7 @@ if False:
     p1y=y0[np.argmax(x0)]
     
 ###Vertical
-if True:
+if False:
     ###Initial point of transect. Default is horizontal
     p0x=x0[np.argmin(y0)]
     p0y=y0[np.argmin(y0)]
@@ -184,7 +213,11 @@ if True:
     p1y=y0[np.argmax(y0)]
 
 
-
+#Let's specify transect coordinates
+p0x=6219784
+p0y=2190613
+p1x=6221597
+p1y=2193674
 
 
 
@@ -193,20 +226,20 @@ welloff=(np.array([len(i) for i in wellname])*0.3).astype("int")
 
 #Library of lithologies
 
-lith=np.array(['CH','CL','LeveeFill', 'OH', 'OL', 'PT','SC', 'SM', 'SP', 'SW'])
+lith=np.array(['CH','CL','LeveeFill', 'OH', 'OL', 'PT','SC', 'SM', 'SP', 'SW','ML'])
 
 #let´s figure out lithologies in this map
 
 #Flat array with lithologies
 lithu=np.array([])
 for sl in range(len(geo)):
-    lithu=np.append(lithu,np.unique(geo[sl]))
+    lithu=np.append(lithu,geo[sl][geo[sl].astype(str)!=str(np.nan)])
 
 lithu=np.unique(lithu[np.where(lithu!='nan')])
 
 #Patterns library
 lithcol=np.array(['url(#CH)','url(#CL)', 'url(#LeveeFill)', 'url(#OH)', 'url(#OL)',\
-        'url(#PT)', 'url(#SC)', 'url(#SM)', 'url(#SP)', 'url(#SW)'])
+        'url(#PT)', 'url(#SC)', 'url(#SM)', 'url(#SP)', 'url(#SW)', 'url(#ML)'])
 
 #Transect  coordinates of logs 
 x=np.array([])
@@ -233,21 +266,27 @@ for well in range(len(x0)):
                  [p0y*(p1x-p0x)-p0x*(p1y-p0y)]])
     A_inv=np.linalg.inv(A)
     xdum=np.matmul(A_inv,b)
-    d_dum=int(np.sqrt(xdum[0]**2)+np.sqrt(xdum[1]**2))
+    d_dum=int(np.sqrt(xdum[0]**2+xdum[1]**2))
     x=np.append(x,d_dum)
 
 GUdata2=np.array([[],[]])
 
+xdums2=np.empty((int(GUdata.shape[1]/2)-1,GUdata.shape[0]))
+xdums2[:]=np.nan
+zdums2=np.empty((int(GUdata.shape[1]/2)-1,GUdata.shape[0]))
+zdums2[:]=np.nan
+
 if Lines:
 ####Let's get the transect coordinates for the layers
     for layer in range(len(Layers[Layers!=''])+1):
+#    for layer in range(len(Layers)):
         xdums=GUdata[:,3*layer]
         xdums=xdums[~np.isnan(xdums)]
         ydums=GUdata[:,3*layer+1]
         ydums=ydums[~np.isnan(ydums)]
         zdums=GUdata[:,3*layer+2]
         zdums=zdums[~np.isnan(zdums)]
-        xdums2=np.array([])
+        xdums2_dum=np.array([])
         for point in range(len(xdums)):
             
             qx=xdums[point]
@@ -258,11 +297,14 @@ if Lines:
                           [p0y*(p1x-p0x)-p0x*(p1y-p0y)]])
             A_inv=np.linalg.inv(A)
             xdum=np.matmul(A_inv,b)
-            d_dum=int(np.sqrt(xdum[0]**2)+np.sqrt(xdum[1]**2))
-            xdums2=np.append(xdums2,d_dum)
-        xdums2=xdums2[np.argsort(xdums2)]
-        zdums=zdums[np.argsort(xdums2)]
-        GUdata2=np.append(GUdata2,np.array([xdums2,zdums]),axis=1).astype('int')
+            d_dum=int(np.sqrt(xdum[0]**2+xdum[1]**2))
+            xdums2_dum=np.append(xdums2_dum,d_dum)
+
+        xdums2_dum=xdums2_dum[np.argsort(xdums2_dum)]
+        zdums=zdums[np.argsort(xdums2_dum)]
+        xdums2[layer]=xdums2_dum
+        zdums2[layer]=zdums
+#        GUdata2=np.append(GUdata2,np.array([xdums2,zdums]),axis=1).astype('int')
 
     
 #min of x
@@ -271,6 +313,9 @@ minx=min(x)
 #max of x
 maxx=max(x)
 
+if Lines:
+    minx=min(minx,np.min(xdums2))
+    maxx =max(maxx,np.max(zdums))
 
 
 #x axis limits
@@ -278,11 +323,13 @@ maxx=max(x)
 xlim=np.array([minx,maxx])
 
 #Min of z
-minz=np.amin(z[np.where(~np.isnan(z))])
+#minz=np.amin(z[np.where(~np.isnan(z))])
+minz=int(ylim[0]/10)*10-5
 
 maxz=np.amax(z[np.where(~np.isnan(z))])
 #z axis limits
-ylim=np.array([minz,maxz]).astype('int')
+#ylim=np.array([round(minz/10)*10,round(maxz/10)*10]).astype('int')
+ylim=np.array([minz,maxz])
 
 #x tickmarks
 
@@ -295,15 +342,17 @@ xlim[0]=xlim[0]
 xticks0=xticks-np.amin(xticks)
 
 #y tickmarks
-dty=10
-yticks=(np.arange(ylim[0]-dty,ylim[1]+2*dty,dty)/10).astype("int")*10
-yticks0=-yticks
+dty=5
+#yticks=(np.arange(ylim[0]-dty,ylim[1]+2*dty,dty)/10).astype("int")*10
+yticks=np.arange(int(ylim[0]),int(ylim[1]),dty)
+#yticks0=-yticks
+yticks0=yticks
 
 
 #Corrected x coordinates of logs
 x2=(x-minx)/(maxx-minx)*xscale+mx-wlw/2
 #Corrected x coordinates of layers
-GUdata2[0]=(GUdata2[0]-minx)/(maxx-minx)*xscale+mx-wlw/2
+#GUdata2[0]=(GUdata2[0]-minx)/(maxx-minx)*xscale+mx-wlw/2
 
 
 #Corrected x coordinates of tickmarks
@@ -317,9 +366,12 @@ w=str(int(max(x2)+1.5*wlw+mxr))
 #Corrected z coordinates of logs
 z2=(z-minz)/(maxz-minz)*yscale+my
 
+
 if Lines:
 #Corrected z coordinates of layers
-    GUdata2[1]=(-GUdata2[1]-minz)/(maxz-minz)*yscale+my
+    zdums2=(zdums2-minz)/(maxz-minz)*yscale+my
+#Corrected x coordinates
+    xdums2=(xdums2-minx)/(maxx-minx)*xscale+mx
 
 
 
@@ -340,7 +392,11 @@ xlim2[0]=xlim2[0]-wlw
 
 #height of domain
 h=str(int(np.amax(z2[np.where(~np.isnan(z2))])+myb))
-
+z2=int(h)-z2
+ylim2=int(h)-ylim2
+yticks=int(h)-yticks
+if Lines:
+    zdums2=int(h)-zdums2
 
 Patterns=open('Patterns.svg',"r")
 Patterns=Patterns.read()
@@ -356,17 +412,30 @@ if Lines:
 ###Let's draw layers
     for layer in range(len(Layers[Layers!=''])):
         #Points for layer
-        lendum=len(GUdata[:,layer])
+        lendum=xdums2.shape[1]
         #Top
-        xdum1=GUdata2[0,range(3*layer,3*layer+3)]
-        zdum1=GUdata2[1,range(3*layer,3*layer+3)]
+        #xdum1=GUdata2[0,range(3*layer,3*layer+3)]
+#        xdum1 = GUdata2[0, layer*lendum: lendum * layer + lendum]
+        xdum1=xdums2[layer]
+        sort_ind_0 = np.argsort(xdum1)
+
+
+        zdum1=zdums2[layer]
+        zdum1 = zdum1[sort_ind_0]
+        xdum1 = np.sort(xdum1)
     #Bottom
-        xdum2=GUdata2[0,range(3*(layer+1),3*(layer+1)+3)]
-        zdum2=GUdata2[1,range(3*(layer+1),3*(layer+1)+3)]
-        points_dum=np.concatenate((np.array([xdum1+wlw/2,zdum1]),np.flip(np.array([xdum2+wlw/2,zdum2]),axis=1)),axis=1).astype('int')
+#        xdum2=GUdata2[0,(layer+1)*lendum: lendum * (layer+1) + lendum]
+        xdum2=xdums2[layer+1]
+        sort_ind=np.argsort(xdum2)[::-1]
+#        zdum2=GUdata2[1,(layer+1)*lendum: lendum * (layer+1) + lendum]
+        zdum2 =zdums2[layer+1]
+        zdum2=zdum2[sort_ind]
+        xdum2=np.sort(xdum2)[::-1]
+        points_dum=np.concatenate((np.array([xdum1,zdum1]),np.array([xdum2,zdum2])),axis=1).astype('int')
         points_dum_str=""
         coldum=lithcol[np.argwhere(lith==Layers[layer])][0][0]
         for i in range(len(points_dum[0])):
+#        for i in range(140):
             points_dum_str=points_dum_str+points_dum[0][i].astype('str')+','+points_dum[1][i].astype('str')+' '
     
         str0=str0+'\n  <polygon points="'+points_dum_str+'"'+' fill="'+coldum+'" fill-opacity="0.6" \n'\
@@ -397,23 +466,23 @@ for well in range(len(np.array(wellname))):
 
             if (len(np.argwhere(lith==litdum))>0)and(~np.isnan(z2[well,layer])):
                 #color of the layer
-                coldum=lithcol[np.argwhere(lith==litdum)]
+                coldum=lithcol[np.argwhere(lith==litdum)][0][0]
                 
                 str0=str0+'\n  <rect x="'+xwelldum+'" y="'+str(int(z2[well,layer]))+'" width="'+str(int(wlw))\
                 +'" height="'+str(int(z2[well,layer+1]-z2[well,layer]))+'"\n'\
-                +'        style="stroke: #000000;\n               fill: '+str(coldum[0][0])+';\n'\
+                +'        style="stroke: #000000;\n               fill: '+str(coldum)+';\n'\
                 +'              "\n'\
                 +'          />\n\n'
     
 #Let's plot x axis
 yxtick=str(np.amax(yticks))
-str0=str0+'\n  <line x1="'+str(xlim2[0])+'" y1="'+yxtick+'" x2="'+\
-str(np.amax(xticks))+'" y2="'+yxtick+'" stroke="black" />\n'
+str0=str0+'\n  <line x1="'+str(xlim2[0])+'" y1="'+str(max(yticks))+'" x2="'+\
+str(int(np.amax(xticks)))+'" y2="'+str(max(yticks))+'" stroke="black" />\n'
 
 #Let's plot y axis
 xytick=str(xlim2[0])
-str0=str0+'\n  <line x1="'+xytick+'" y1="'+str(np.amin(yticks))+'" x2="'+\
-xytick+'" y2="'+yxtick+'" stroke="black" />\n'
+str0=str0+'\n  <line x1="'+xytick+'" y1="'+str(ylim2[1])+'" x2="'+\
+xytick+'" y2="'+str(ylim2[0])+'" stroke="black" />\n'
 
 #lets draw x tickmarks
 for tick in range(len(xticks)):
