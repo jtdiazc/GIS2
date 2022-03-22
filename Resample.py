2590e046bca27f2748a2e6af304136a8f550b3ea
jtdiazc <jdiaz@hydrofocus.com>
2022-03-22 14:22:16 -0700

    Just parametrized some variables

--- Resample.py
+++ Resample.py
@@ -5,36 +5,33 @@ from osgeo import gdal
 import geopandas as gpd
 
 #Raster dir
-ras_dir=r"\\hydro-nas\Team\Projects\5635_SFEI Carbon and GHG\GIS\raster"
+ras_dir=r"\\hydro-nas\Team\Projects\5630_DSC\GIS\raster\Paper"
 
 #Vector dir
 vec_dir=r"\\hydro-nas\Team\Projects\5635_SFEI Carbon and GHG\GIS\vector"
 
-template = gdal.Open(os.path.join(ras_dir,'Layout_Domain.tif'))
+template_name="DEM_MBK_ft_NGVD29_26910.tif"
+
+#New resolution
+new_res=5
+
+#Input name
+input_nam="DEM_MBK_ft_NGVD29_26910"
+
+template = gdal.Open(os.path.join(ras_dir,template_name))
 ulx, xres, xskew, uly, yskew, yres  = template.GetGeoTransform()
 lrx = ulx + (template.RasterXSize * xres)
 lry = uly + (template.RasterYSize * yres)
 #temp_cell_size=template.transform[0]
 
-infn = os.path.join(ras_dir,'TopoBathy_26910.tif')
-outfn = os.path.join(ras_dir,'TopoBathy_26910_10m.tif')
+infn = os.path.join(ras_dir,input_nam+".tif")
+outfn = os.path.join(ras_dir,input_nam+"_"+str(new_res)+".tif")
 
-xres=10
-yres=10
+
 resample_alg = 'average'
 
-options = gdal.WarpOptions(options=['tr'], xRes=xres, yRes=yres,resampleAlg=resample_alg,multithread=True, outputBounds = [ulx, uly, lrx, lry])
+options = gdal.WarpOptions(options=['tr'], xRes=new_res, yRes=new_res,resampleAlg=resample_alg,multithread=True, outputBounds = [ulx, uly, lrx, lry])
 
 ds = gdal.Warp(outfn, infn, options=options)
 ds = None
 
-#pts = gpd.read_file(os.path.join(vec_dir,'Template_Sampling_Points.shp'))
-#pts = pts[['UTM_E', 'UTM_N', 'Value', 'geometry']]
-#pts.index = range(len(pts))
-#coords = [(x,y) for x, y in zip(pts.UTM_E, pts.UTM_N)]
-
-src=rasterio.open(outfn)
-
-# Sample the raster at every point location and store values in DataFrame
-pts['Raster Value'] = [x for x in src.sample(coords)]
-pts['Raster Value'] = probes.apply(lambda x: x['Raster Value'][0], axis=1)
