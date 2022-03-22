6c6eb7dcce4a83338e8879188ec1d30edbbee29f
jtdiazc <jdiaz@hydrofocus.com>
2022-03-22 14:38:40 -0700

    Set no data value for output

--- Resample.py
+++ Resample.py
@@ -18,6 +18,8 @@ new_res=5
 #Input name
 input_nam="DEM_MBK_ft_NGVD29_26910"
 
+no_data_ouput=999
+
 template = gdal.Open(os.path.join(ras_dir,template_name))
 ulx, xres, xskew, uly, yskew, yres  = template.GetGeoTransform()
 lrx = ulx + (template.RasterXSize * xres)
@@ -30,7 +32,8 @@ outfn = os.path.join(ras_dir,input_nam+"_"+str(new_res)+".tif")
 
 resample_alg = 'average'
 
-options = gdal.WarpOptions(options=['tr'], xRes=new_res, yRes=new_res,resampleAlg=resample_alg,multithread=True, outputBounds = [ulx, uly, lrx, lry])
+options = gdal.WarpOptions(options=['tr'], xRes=new_res, yRes=new_res,resampleAlg=resample_alg,multithread=True,
+                           outputBounds = [ulx, uly, lrx, lry],dstNodata= no_data_ouput)
 
 ds = gdal.Warp(outfn, infn, options=options)
 ds = None
